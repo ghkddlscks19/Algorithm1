1910b8ffbea253a6d9f554d4af8e2022433a1d2a
황인찬 <ghkddlscks19@naver.com>
2022-01-12 22:29:12 +0900

    20220112 두수의 덧셈문제

---
+++ src/sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sum {
+    public static int add(int a, int b){
+        int res = a+b;
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(add(n,m));
+    }
+}
