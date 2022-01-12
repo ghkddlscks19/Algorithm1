1c15467b54665df3fdfc1219264b37da765f598d
황인찬 <ghkddlscks19@naver.com>
2022-01-12 22:29:41 +0900

    20220112 하나의 입력받은 정수 만큼 두 수의 덧셈

---
+++ src/sum1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i =0; i<T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int res = a+b;
+            System.out.println(res);
+        }
+    }
+}
