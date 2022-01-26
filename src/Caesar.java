30df523e28231cd2e42d8e0dd1b5ff6704b520e4
황인찬 <ghkddlscks19@naver.com>
2022-01-26 22:22:28 +0900

    20220126 시저암호 수정

--- src/Caesar.java
+++ src/Caesar.java
@@ -5,6 +5,9 @@ public class Caesar {
         String answer = "";
         for(int i =0; i<s.length(); i++){
             char ch = s.charAt(i);
+            if(ch == ' '){
+                answer += ch;
+            }
             if(ch>='a' && ch<='z'){
                 if(ch+n>'z'){
                     answer += (char)(ch-26+n);
